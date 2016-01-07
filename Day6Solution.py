0c90a19b7ee16423171e1e7193af3ef7ad436059
Vitali's Repo <vkremez@users.noreply.github.com>
2016-01-06 22:32:39 -0500

    Create Day6Solution.py

---
+++ Day6Solution.py
@@ -0,0 +1,43 @@
+''' 
+
+Solution for HackerRank Competition
+
+Problem Statement
+
+Welcome to Day 6! Review everything we've learned so far by making a MadLibs program in this video, or just jump right into the problem.
+
+Your teacher has given you the task of drawing a staircase structure. Being an expert programmer, you decided to make a program to draw it for you instead. Given the required height, can you print a staircase as shown in the example? 
+Note: The last line has zero spaces before it.
+
+Good luck!
+
+Constraints 
+1≤N≤100
+Input Format
+
+You are given an integer N depicting the height of the staircase.
+
+Output Format
+
+Print a staircase of height N that consists of # symbols and spaces as given in Sample Output.
+
+Sample Input
+
+6
+
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+
+'''
+
+#!/bin/env python
+
+n = int(raw_input().strip())
+for i in range(1,n+1): 
+    print " "*(n-i)+"#"*i
